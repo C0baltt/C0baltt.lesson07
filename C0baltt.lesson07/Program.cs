071229259193805d53f916343d694c6b209f65d0
Александр Жаворонок <C0baltt@live.com>
2020-09-13 16:40:25 +0300

    2

--- C0baltt.lesson07/Program.cs
+++ C0baltt.lesson07/Program.cs
@@ -3,39 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*создать коллекцию целых чисел (15 элементов)
+ заполнить данными от пользователя (с проверкой)
+посчитать сумму всех элементов коллекции (for)
+посчитать разность всех элементов (while)
+посчитать произведение (do while)
+вывести коллекцию(foreach)*/
 namespace C0baltt.lesson07
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] array = new int[10, 10];
-            for (var i = 0; i < 10; i++)
+            var list = new List<string>();//инициализируем коллекцию list
+            for (var i = 0; i < 15; i++)//добавляем 15 элементов в коллекцию
             {
-                for (var j = 0; j< 10; j++)
-                {
-                    array[i, j] = 8;
-                }
-            }
-            foreach(var item in list)//(элемент in коллекция)
-            {
-                Console.WriteLine(item);
+                list.Add(i);//добавляем элементы в коллекцию
             }
 
-           
-
-            Console.ReadLine();
+            for (var i = 0; i < list.Count; i++)//вводим значения для 15-ти элементов коллекции list 
+            {
+                Console.WriteLine(list[i]);//считываем с консоли значение i-того элемента
+            }    
+                Console.ReadLine();
         }
     }
 }
-/*
+/*          МАТЕРИАЛЫ ПО УРОКУ
  var list = new List<string>();
             while (list.Count < 5)
             {
                 list.Add(Console.ReadLine());
             }
             var i = 0;
+
             do
             {
                 Console.WriteLine(list[i]);
